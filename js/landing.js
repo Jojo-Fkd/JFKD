9809b3f586c6749b52d906109944cd7a3eaa5004
Jonathan Fekadu <jonathanfekadu1@gmail.com>
2024-05-29 23:42:19 +0300

    committted

--- js/landing.js
+++ js/landing.js
@@ -38,10 +38,51 @@ becomeClientBtn.onclick = () => {
   );
   options.forEach((option) => {
     option.onclick = () => {
+      const formBtn = popupContainer.querySelector(".request form button");
+      formBtn.disabled = false;
       options.forEach((option) => {
         option.className = "";
       });
       option.className = "chosen";
+
+      // SUBMISSION OF REQUEST
+
+      const form = popupContainer.querySelector(".request form");
+      form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let params = {
+          from_name: form.querySelector("#name-establishment").value,
+          from_email: form.querySelector("#email").value,
+          from_type: option.innerHTML,
+        };
+
+        emailjs
+          .send("service_7z6ushb", "template_nvw89rq", params)
+          .then(() => {
+            popupContainer.querySelector(".request").style.display = "none";
+            const successPopup =
+              popupContainer.querySelector(".request-success");
+            successPopup.classList.add("success");
+            successPopup.querySelector("button").onclick = () => {
+              location.reload();
+            };
+          })
+          .catch(() => {
+            alert("Failed, try again");
+            location.reload();
+          });
+      });
     };
   });
+};
+
+/* MOBILE NAVIGATION ACTIVATION */
+
+const dropIcon = document.querySelector(".drop-icon");
+const navMobile = document.querySelector(".nav-mobile");
+
+dropIcon.onclick = () => {
+  dropIcon.classList.toggle("active");
+  navMobile.classList.toggle("active");
+  body.classList.toggle("hidden");
 };
